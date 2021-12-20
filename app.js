b132fdb3f391296a23cc02668959b6b3e3606072
Albert <albertlee0622@gmail.com>
2021-12-20 05:04:32 -0500

    practice JS

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("hi");
+// alert("hi");
+const a = 6;
+const b = 3;
+
+let myName = "Albert";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "Seung-jun";
+console.log("hello " + myName);
