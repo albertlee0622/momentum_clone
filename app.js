fa0ec9f26501e3f71c385752dcacb88cdb79699b
Albert <albertlee0622@gmail.com>
2021-12-20 05:47:14 -0500

    practice JS2

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 // alert("hi");
-const a = 6;
-const b = 3;
+// var a = 6;
+// var b = 3;
 
-let myName = "Albert";
+// let myName = "Albert";
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log("hello " + myName);
 
-myName = "Seung-jun";
-console.log("hello " + myName);
+// myName = "Seung-jun";
+// console.log("hello " + myName);
+
+// const amIFat = null;
+// let something;
+// console.log(something, amIFat);
+
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// const thu = "thu";
+// const fri = "fri";
+// const sat = "sat";
+// const sun = "sun";
+
+// const daysOfWeek = [mon, tue, wed, thu, fri, sat];
+// console.log(daysOfWeek);
+// daysOfWeek.push(sun);
+// console.log(daysOfWeek);
+// const nonsense = ["Albert", 3, false, null];
+// console.log(nonsense);
+
+//assignment is now allowed for const, but const array can be appended an item
+const toBuy = ["coffee"];
+toBuy.push("banana");
+console.log(toBuy);
