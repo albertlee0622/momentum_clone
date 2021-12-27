1633dd3bd4306f079e37711405567dc5015ca7c9
Albert Lee <alee870622@gmail.com>
2021-12-26 20:19:12 -0500

    add HTML form

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const loginButton  = document.querySelector("#login-form button");
 function onLoginBtnClick() {
     const username = loginInput.value;
     const limit = 15;
-    if(username === "") {
-        alert("Please write your name");
-    }
-    else if(username.length >= limit) {
-        alert(`Username must be less than ${limit}`);
-    }
+    // if(username === "") {
+    //     alert("Please write your name");
+    // }
+    // else if(username.length >= limit) {
+    //     alert(`Username must be less than ${limit}`);
+    //     loginInput.value = "";
+    // }
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
 //4.1 3:45
+//we want to stop the form submitted after carriage return because it refreshes the entire webpage(slow) 
