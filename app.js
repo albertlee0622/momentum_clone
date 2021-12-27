37d475f941c1b3dbb1e2474cd2141edec2f0aa0d
Albert Lee <alee870622@gmail.com>
2021-12-27 07:12:46 -0500

    practice localStorage JS

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput  = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+// const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 // const loginButton  = document.querySelector("#login-form button");
+function displayGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove("hidden");
+}
 
 function onLoginSubmit(event) {
-    const username = loginInput.value;
     event.preventDefault();
-    console.log(username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    displayGreeting(username);
+    // greeting.classList.remove(HIDDEN_CLASSNAME);
+    // greeting.innerText = `Hello ${username}`;
+    localStorage.setItem(USERNAME_KEY, username);
     // const limit = 15;
     // if(username === "") {
     //     alert("Please write your name");
@@ -22,10 +34,19 @@ function onLinkClick(event) {
     console.log(event);
 }
 
+
+
 // loginButton.addEventListener("click", onLoginBtnClick);
 //4.1 3:45
 //we want to stop the form submitted after carriage return because it refreshes the entire webpage(slow)
 //get the input value when submitted
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", onLinkClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+    displayGreeting(savedUsername);
+}
+// link.addEventListener("click", onLinkClick);
 
