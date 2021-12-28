70387e9e8552a2dbdbb912d66abac4049760499d
Albert Lee <alee870622@gmail.com>
2021-12-28 00:37:21 -0500

    add 10 quotes

--- js/app.js
+++ js/app.js
@@ -50,3 +50,78 @@ else {
 }
 // link.addEventListener("click", onLinkClick);
 
+const quotes = [
+    {
+        "quote": "Great things are not done by impulse, but by a series of small things brought together.",
+        "author": "George Eliot"  
+    },
+    {
+        "quote": "Keep on going and the chances are you will stumble on something, perhaps when you are least expecting it. I have never heard of anyone stumbling on something sitting down."
+        ,
+        "author": "Charles F. Kettering"  
+    },
+    {
+        "quote": "There is no better than adversity. Every defeat, every heartbreak, every loss, contains its own seed, its own lesson on how to improve your performance the next time."
+        ,
+        "author": "Og Mandino"
+ 
+    },
+    {
+        "quote": "Live out your imagination instead of out of your memory.",
+        "author": "Les Brown"
+    },
+    {
+        "quote": "You can accomplish virtually anything if you want it badly enough and if you are willing to work long enough and hard enough.",
+        "author": "Brian Tracy"
+    },
+    {
+        "quote": "All men make mistakes, but only wise men learn from their mistakes.",
+        "author": "Winston Churchill"
+    },
+    {
+        "quote": "When you believe in a thing, believe in it all the way, implicitly and unquestionable.",
+        "author": "Walt Disney"
+    },
+    {
+        "quote": "There isn't a person anywhere that isn't capable of doing more than he thinks he can.",
+        "author": "Henry Ford"
+    },
+    {
+        "quote": "Opportunities are like sunrises - if you wait too long, you miss them.",
+        "author": "William Arthur Ward"
+    },
+    {
+        "quote": "People with goals succeed because they know where they're going.",
+        "author": "Earl Nightingale"
+    },
+]
+console.log(quotes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
