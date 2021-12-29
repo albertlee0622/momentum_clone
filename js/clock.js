a4d68d8112e0d0e87ef06275bbd7649425431337
Albert Lee <alee870622@gmail.com>
2021-12-29 04:45:51 -0500

    add quotes in HTML

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,5 @@
 const clock = document.querySelector("h2#clock");
-console.dir(clock);
+// console.dir(clock);
 // interval: something that happens every k second
 function addLeadingZero(number) {
     return String(number).padStart(2, '0');
