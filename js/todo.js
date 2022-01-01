dd4a931be6e9a2438315d6b5850a3aa0ee12221b
Albert Lee <alee870622@gmail.com>
2022-01-01 04:54:34 -0500

    fixed addTodo

--- js/todo.js
+++ js/todo.js
@@ -1,64 +1,74 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
-const todoStr = localStorage.getItem("todos");
-let todoArray = null;
+let todoStr = localStorage.getItem("todos");
+let todoArray = [];
 const todos = [];
 
-function displayTodo() {
+function updateTodoArray() {
+    todoStr = localStorage.getItem("todos");
     if(todoStr !== null) {
         todoArray = todoStr.split(',');
     }
-    todoArray.forEach(addTodo);
+    else {
+        todoArray = [];
+    }
+}
+
+function displayTodo(newTodo) {
+    //add button for the li
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    li.appendChild(span);
+    toDoList.appendChild(li);
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    li.appendChild(button);
 }
 
 function deleteTodo(event) {
     const li = event.target.parentElement;
     li.remove();
-    localStorage.removeItem("todos");
     const todo = li.querySelector("span");
+    updateTodoArray();
     for(let i = 0; i < todoArray.length; i++) {
         if(todoArray[i] == todo.innerText) {
             console.log(todoArray)
             todoArray.splice(i, 1);
             console.log(todoArray);
+            localStorage.removeItem("todos");
             localStorage.setItem("todos", todoArray);
         }
     }
 }
 
 function handleButtonClick(event) {
-    event.preventDefault();
+    // event.preventDefault();
     deleteTodo(event);
 }
 
-function addTodo(newTodo) {
-    //creat li for ul
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    span.innerText = newTodo;
-    li.appendChild(span);
-    toDoList.appendChild(li);
-
-    //add button for the li
-    const button = document.createElement("button");
-    button.innerText = "❌";
-    li.appendChild(button);
-    toDoList.addEventListener("click", handleButtonClick);
-
-    //save todo in local storage
-    todos.push(newTodo);
-    localStorage.setItem("todos", todos);
+function saveTodo() {
+    //save todo in local storage using JSON.stringify
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    addTodo(newTodo);
+    todos.push(newTodo);
+    displayTodo(newTodo);
+    saveTodo();
 }
 
-if(todoStr !== null) {
-    displayTodo();
-}
+// if(todoStr !== null) {
+//     displayTodo();
+// }
+
 toDoForm.addEventListener("submit", handleToDoSubmit);
+toDoList.addEventListener("click", handleButtonClick);
+
+//I need to find the right data structure to save todos in local storage
+//JSON.stringify();
+//now how to retrieve array from stringified data in local storage
